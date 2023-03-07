deb460e25da3f878a3d7e72d964b466797d08192
Sammy-T <cyam9019@aim.com>
2023-03-07 03:40:12 -0500

    Create coin pickup

--- scripts/level.gd
+++ scripts/level.gd
@@ -3,10 +3,15 @@ extends Node2D
 
 @export var player: CharacterBody2D
 
+var coin_count: int = 0
+
 
 # Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-#	pass # Replace with function body.
+func _ready() -> void:
+	var coins: Array[Node] = get_tree().get_nodes_in_group("coin")
+	
+	for coin in coins:
+		coin.coin_collected.connect(_update_coin_count)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,3 +24,8 @@ func _update_debug() -> void:
 		return
 	
 	%PlayerVelocity.text = "Velocity: %s" % Vector2i(player.velocity)
+
+
+func _update_coin_count() -> void:
+	coin_count += 1
+	%CoinCount.text = "x%d" % coin_count

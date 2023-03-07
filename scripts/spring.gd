8b8fc15badb001197dbb0c2ff6f3cb66e0204d12
Sammy-T <cyam9019@aim.com>
2023-03-07 07:50:10 -0500

    Create spring

---
+++ scripts/spring.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+
+const SPRING_VELOCITY: float = -700.0
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+#	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if !body is Player:
+		printerr("%s Error: Found %s instead of Player" % [self.name, body])
+		return
+	
+	%AnimatedSprite2D.play("default") # Play the bounce animation
+	
+	body.velocity.y = SPRING_VELOCITY # Use the spring's velocity to bounce the player

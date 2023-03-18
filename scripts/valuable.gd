8f0e9374050e5bfbb4cb1644aadfe33b273fde2b
Sammy-T <cyam9019@aim.com>
2023-03-18 09:02:13 -0400

    Play pickup sfx

--- scripts/valuable.gd
+++ scripts/valuable.gd
@@ -6,6 +6,7 @@ signal valuable_collected(value: int)
 @export var value: int = 1
 
 @onready var anim_player: AnimationPlayer = %AnimationPlayer
+@onready var sfx_pickup: AudioStreamPlayer = %Pickup
 
 
 # Called when the node enters the scene tree for the first time.
@@ -20,6 +21,8 @@ signal valuable_collected(value: int)
 
 func _on_body_entered(_body: Node2D) -> void:
 	valuable_collected.emit(value)
+	
+	sfx_pickup.play() # Play the pickup sfx
 	
 	if anim_player.has_animation("collect"):
 		anim_player.play("collect")
